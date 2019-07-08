7fe98b37e8a34257f585f3557277a34554ca46bd
Mike <m.collado.gomez@gmail.com>
2019-07-08 19:52:02 +0200

    WIP

--- model/models.py
+++ model/models.py
@@ -1,21 +1,46 @@
 from django.db import models
 
 
-class Genre(models.Model):
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+
+class Genre(BaseModel):
     name = models.CharField(max_length=512, db_index=True)
 
 
-class AudiovisualRecord(models.Model):
+class Person(BaseModel):
+    name = models.CharField(max_length=512, db_index=True)
+
+
+class AudiovisualRecord(BaseModel):
+    """
+    TODO
+    Nos falta fuente de descarga
+    """
     name = models.CharField(max_length=512)
     genres = models.ManyToManyField(Genre, related_name='audiovisual_records')
+    year = models.IntegerField(default=0)
+
+    directors = models.ManyToManyField(Person, related_name='audiovisual_records_as_director')
+    writers = models.ManyToManyField(Person, related_name='audiovisual_records_as_writer')
+    stars = models.ManyToManyField(Person, related_name='audiovisual_records_as_star')
 
     deleted = models.BooleanField(default=False, db_index=True)
     disabled = models.BooleanField(default=False, db_index=True)
+
+    last_check = models.DateTimeField(blank=True, null=True)
 
     def delete(self, using=None, keep_parents=False):
         self.deleted = True
         self.save()
 
 
-class Score(models.Model):
+class Score(BaseModel):
     audiovisual_record = models.ForeignKey(AudiovisualRecord, on_delete=models.PROTECT, related_name='scores')
+
+
+class Photo(BaseModel):
+    audiovisual_record = models.ForeignKey(AudiovisualRecord, on_delete=models.PROTECT, related_name='photos')
+    image = models.ImageField(blank=False, null=False)
