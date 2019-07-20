49d7ddd49818f037961eb865324a08c421d61790
Mike <m.collado.gomez@gmail.com>
2019-07-20 21:33:15 +0200

    Refactor for downloads

--- core/robots/downloads.py
+++ core/robots/downloads.py
@@ -14,36 +14,39 @@ from concurrent.futures.thread import ThreadPoolExecutor
 
 @execute_each(interval='1-minute')
 async def compile_download_links_from_audiovisual_records():
-    print('[ STARTED] compile_download_links_from_audiovisual_records')
-    configuration, from_dt = _get_ts_configuration('last_download_fetched')
-    audiovisual_records = (
-        Search
-        .Builder
-        .new_search(AudiovisualRecord)
-        .add_condition(Condition('deleted', Condition.OPERATOR_EQUALS, False))
-        .add_condition(Condition('general_information_fetched', Condition.OPERATOR_EQUALS, True))
-        .add_condition(Condition('created_date', Condition.OPERATOR_GREAT_THAN, from_dt))
-        .search()
-    )
-
+    configuration = _get_ts_configuration('last_download_fetched')
     with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = []
-        for audiovisual_record in audiovisual_records:
-            for source_class in get_all_download_sources():
+        for source_class in get_all_download_sources():
+            source_name = source_class.source_name
+            ts = configuration.data.get(source_name, 0)
+            from_dt = datetime.utcfromtimestamp(ts).replace(tzinfo=timezone.utc)
+
+            audiovisual_records = (
+                Search
+                .Builder
+                .new_search(AudiovisualRecord)
+                .add_condition(Condition('deleted', Condition.OPERATOR_EQUALS, False))
+                .add_condition(Condition('general_information_fetched', Condition.OPERATOR_EQUALS, True))
+                .add_condition(Condition('created_date', Condition.OPERATOR_GREAT_THAN, from_dt))
+                .search()
+            )
+
+            futures = []
+            for audiovisual_record in audiovisual_records:
                 future = executor.submit(_refresh_download_results_from_source, audiovisual_record, source_class)
                 futures.append(future)
-            if audiovisual_record.created_date > from_dt:
-                from_dt = audiovisual_record.created_date
-        configuration.data['ts'] = from_dt.timestamp()
-        configuration.save()
+                if audiovisual_record.created_date > from_dt:
+                    from_dt = audiovisual_record.created_date
+            configuration.data[source_name] = from_dt.timestamp()
+
         for future in concurrent.futures.as_completed(futures):
             future.result()
-        print('[FINISHED] compile_download_links_from_audiovisual_records')
+        configuration.save()
 
 
-#@execute_each(interval='1-minute')
+@execute_each(interval='1-minute')
 async def compile_expired_download_links():
-    n_days_ago = datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(days=120)
+    n_days_ago = datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(days=60)
     download_results = (
         Search
         .Builder
@@ -54,6 +57,8 @@ async def compile_expired_download_links():
     for download_result in download_results:
         source_class = get_download_source_by_name(download_result.source_name)
         audiovisual_record = download_result.audiovisual_record
+        # TODO si la película es de este año, tocará actualizar los enlaces más a menudo,
+        # TODO si la película es de hace años, es tontería. Quizá revisar links, por si alguno tiene un 404
         _refresh_download_results_from_source(audiovisual_record, source_class)
 
 
@@ -87,6 +92,4 @@ def _get_ts_configuration(key):
     configuration = Configuration.get_configuration(key=key)
     if configuration is None:
         configuration = Configuration(key=key, data={})
-    ts = configuration.data.get('ts', 0)
-    ts = datetime.utcfromtimestamp(ts).replace(tzinfo=timezone.utc)
-    return configuration, ts
+    return configuration
