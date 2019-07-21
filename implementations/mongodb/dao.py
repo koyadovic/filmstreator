f02324d71b3a406bfd262b7050299b2356b38a5d
Mike <m.collado.gomez@gmail.com>
2019-07-21 07:17:18 +0200

    Check if slugs exist before save an audiovisual record

--- implementations/mongodb/dao.py
+++ implementations/mongodb/dao.py
@@ -32,6 +32,7 @@ class DAOMongoDB(DAOInterface):
         dict_obj = dict(record)
         collection = self._get_collection(MongoAudiovisualRecord)
         _id = dict_obj.pop('_id', None)
+        _check_audiovisual_slug(dict_obj, collection)
         if not _id:
             record._id = collection.insert_one(dict_obj)
         else:
@@ -120,3 +121,19 @@ class DAOMongoDB(DAOInterface):
 
     def _get_collection(self, cls):
         return self._db[cls.collection_name]
+
+
+def _check_audiovisual_slug(dict_obj, collection):
+    n = 0
+    current_slug = dict_obj.get('slug')
+    modified_slug = current_slug
+    is_slug_repeated = True
+    while is_slug_repeated:
+        if n > 0:
+            modified_slug = current_slug + f'-{n}'
+        slug_results = collection.find({'slug': modified_slug})
+        is_slug_repeated = len(slug_results) == 0
+        n += 1
+
+    if modified_slug != current_slug:
+        dict_obj['slug'] = modified_slug
