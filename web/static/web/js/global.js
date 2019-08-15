072a531ab0ef9b424fcf404becf92f41de89197f
Mike <m.collado.gomez@gmail.com>
2019-08-15 17:28:15 +0200

    Random Ads

--- web/static/web/js/global.js
+++ web/static/web/js/global.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
   $("*").on("click",function(){
     var r = Math.floor((Math.random() * 200) + 1);
     if (r === 100) {
-        console.log('TRIGGERED');
-      //window.open('www.yourdomain.com','_blank');
+      window.open('http://deloplen.com/afu.php?zoneid=2772265','_blank');
     }
   });
 });
