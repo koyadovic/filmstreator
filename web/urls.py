0235e63c64a55983275edd71768969bfa35ec47c
Mike <m.collado.gomez@gmail.com>
2019-08-16 17:00:17 +0200

    robots.txt and humans.txt

--- web/urls.py
+++ web/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, re_path
 from web import views
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 
 favicon_view = RedirectView.as_view(url='/static/web/img/favicon.png', permanent=True)
 
 
 urlpatterns = [
     re_path(r'^favicon\.ico$', favicon_view),
+    re_path(r'^robots\.txt$', TemplateView.as_view(template_name="web/robots.txt", content_type='text/plain')),
+    re_path(r'^humans\.txt$', TemplateView.as_view(template_name="web/humans.txt", content_type='text/plain')),
 
     path('dmca/', views.dmca),
     path('404/', views.page404),
