a75a3e10bf0facb9652b46567e1be7fb1b1475e2
Mike <m.collado.gomez@gmail.com>
2019-08-18 19:48:42 +0200

    Remove ads

--- web/static/web/js/global.js
+++ web/static/web/js/global.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("*").on("click",function(){
     var r = Math.floor((Math.random() * 200) + 1);
     if (r === 100) {
-      window.open('http://deloplen.com/afu.php?zoneid=2772265','_blank');
+      //window.open('http://deloplen.com/afu.php?zoneid=2772265','_blank');
     }
   });
 });
