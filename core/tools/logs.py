a8d98e307b60ac96237c061a04485ef62f1fdec6
Miguel <m.collado.gomez@gmail.com>
2019-08-19 10:24:38 +0200

    WIP

--- core/tools/logs.py
+++ core/tools/logs.py
@@ -1,13 +1,22 @@
-import traceback
-
 from sentry_sdk import capture_exception, capture_message
+import traceback
+import logging
+
 
 # By now, we integrate Sentry here
+file_logger = logging.getLogger('filmstreator')
 
 
-def log_message(message: str):
+def log_message(message: str, only_file=False):
+    file_logger.info(message)
+    if only_file:
+        return
     capture_message(message)
 
 
-def log_exception(exception: Exception):
+def log_exception(exception: Exception, only_file=False):
+    file_logger.exception(exception)
+    file_logger.error(traceback.format_exc())
+    if only_file:
+        return
     capture_exception(exception)
