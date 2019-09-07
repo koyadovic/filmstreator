42129e83bc895aad3ba785dd528c5da437980a2e
Mike <m.collado.gomez@gmail.com>
2019-09-07 13:16:55 +0200

    ordering

--- web/views.py
+++ web/views.py
@@ -32,7 +32,7 @@ def landing(request):
             'deleted': False, 'has_downloads': True, 'general_information_fetched': True,
             'global_score__gte': 1.0
         },
-        sort_by=['-year', '-updated_date', '-global_score'],
+        sort_by=['-year', 'updated_date', '-global_score'],
         page_size=30, page=1, paginate=True
     ).get('results')
 
